913016dbc28a5102f9f97bff802087cf113e0699
youyusuf <youyusuf21@gmail.com>
2025-12-29 21:17:58 +0300

    Add initial Go project files and examples
    - Create .gitignore to exclude IDE and build files
    - Add go.mod for module definition
    - Implement a simple "Hello, World!" program in helloworld.go
    - Document learning outcomes in we-learned.md
    - Introduce variables and their usage in variables.go
    - Expand variable documentation in variables/we-learned.md

---
+++ helloworld/helloworld.go
@@ -0,0 +1,6 @@
+package main // declare the main package , it is the starting point of the program
+import "fmt" // import the fmt package for formatted I/O
+
+func main() { // main function, the entry point of the program
+	fmt.Println("Hello, World!") // print "Hello, World!" to the console
+}
